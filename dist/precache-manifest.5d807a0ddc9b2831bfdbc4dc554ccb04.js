59e6c8ab20f9796c75abaf2640a44ab8f08d50ea
Nicolas <nambroise@3441721.no-reply.drupal.org>
2021-07-04 12:48:31 +0200

    Add new style of card

---
+++ dist/precache-manifest.5d807a0ddc9b2831bfdbc4dc554ccb04.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cd322492fecbe2a50c5f7353947239",
+    "url": "/avatar/AmbroiseNicolas2019_300px.png"
+  },
+  {
+    "revision": "b81e1719770e5b3cb1bbc3c310e41fd0",
+    "url": "/avatar/AmbroiseNicolas2019_300px.webp"
+  },
+  {
+    "revision": "5cea352da23cd4e31959a241bb710272",
+    "url": "/avatar/AmbroiseNicolas2019_400px.png"
+  },
+  {
+    "revision": "c92512ab1ab7b7cd492cf9441168b2f4",
+    "url": "/avatar/AmbroiseNicolas2019_400px.webp"
+  },
+  {
+    "revision": "67d0146e9ed18dd92038e3ca01a4d0bd",
+    "url": "/avatar/AmbroiseNicolas2019_600px.png"
+  },
+  {
+    "revision": "c26d9b59ffb7cdff4e897c2afda76bae",
+    "url": "/avatar/AmbroiseNicolas2019_600px.webp"
+  },
+  {
+    "revision": "63846b1ff8dbef6d89250c9e3b829005",
+    "url": "/avatar/AmbroiseNicolas2020Square100.png"
+  },
+  {
+    "revision": "4be13af90d930987f9b32db4217c8eb8",
+    "url": "/avatar/AmbroiseNicolas2020Square100.webp"
+  },
+  {
+    "revision": "3c2bb4640bff7a995fa7fe0c28aae0f3",
+    "url": "/avatar/AmbroiseNicolas2020Square150.png"
+  },
+  {
+    "revision": "18923325cb85c1dd3a11be35e3b7d73d",
+    "url": "/avatar/AmbroiseNicolas2020Square150.webp"
+  },
+  {
+    "revision": "a0cb25b7f92b4dd759d4354a1ef1a360",
+    "url": "/avatar/AmbroiseNicolas2020Square200.png"
+  },
+  {
+    "revision": "a931fd63e56385c25b7682d08e1efaa9",
+    "url": "/avatar/AmbroiseNicolas2020Square200.webp"
+  },
+  {
+    "revision": "59e0faccd2ce71b75a2aa1566c72e2e1",
+    "url": "/avatar/AmbroiseNicolas2020Square300.png"
+  },
+  {
+    "revision": "05ad9f7d93d58ba3a800fa2426274f46",
+    "url": "/avatar/AmbroiseNicolas2020Square300.webp"
+  },
+  {
+    "revision": "bd4966abfb75ac20d6bca2711d54ba5a",
+    "url": "/avatar/AmbroiseNicolas2020Square500.png"
+  },
+  {
+    "revision": "6a5b24bcea401fd0dabad9761658824c",
+    "url": "/avatar/AmbroiseNicolas2020Square500.webp"
+  },
+  {
+    "revision": "471d8ad99ebaeef8dcea",
+    "url": "/css/app.88b6d762.css"
+  },
+  {
+    "revision": "ceb64d710a1502d499a5",
+    "url": "/css/chunk-vendors.a2cadc9e.css"
+  },
+  {
+    "revision": "ff044bdaf80ec179a3dc82ef03f5f21e",
+    "url": "/img/techno.jpg"
+  },
+  {
+    "revision": "acd55ff46900081c21577a80ee3e7218",
+    "url": "/img/techno.min.jpg"
+  },
+  {
+    "revision": "3ffeac3f8da4c7ccfaa7421cfb04c9d5",
+    "url": "/img/techno2.min.jpg"
+  },
+  {
+    "revision": "369631bc5c1144d109b281dfeba3ee82",
+    "url": "/img/techno2@2x.min.jpg"
+  },
+  {
+    "revision": "cdd680b32e39ff811d16595d33b724a3",
+    "url": "/img/techno@2x.min.jpg"
+  },
+  {
+    "revision": "d6c33137c12053aad431f7e32d53cd5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "471d8ad99ebaeef8dcea",
+    "url": "/js/app.766f10b0.js"
+  },
+  {
+    "revision": "ceb64d710a1502d499a5",
+    "url": "/js/chunk-vendors.801160b6.js"
+  },
+  {
+    "revision": "b49b42f8af7c963e676e1de52920b9c9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b22860b997b155ee3030cfeac906e654",
+    "url": "/sitemap.xml"
+  }
+]);
